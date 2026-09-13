7a93f6414ce37ae6f5f3780d8daa3d078fbe2d65
suraj <sc189708@gmail.com>
2026-09-13 19:26:53 +0530

    fix domain name

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const contactLimter = rateLimit ({
 //Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-    ? 'https://my-portfolio-tau-ten-38.vercel.app'
+    ? 'https://surajportfolio-woad-kappa.vercel.app/'
     : 'http://localhost:5173',
 }));
 app.use(express.json());
